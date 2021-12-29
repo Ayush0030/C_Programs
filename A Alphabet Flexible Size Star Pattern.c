944244db402cc8d6bc36721997463a6f4a08439a
AYUSH DHIMAN <ayushdhiman333@gmail.com>
2021-12-29 23:38:29 +0530

    A Alphabet Flexible Size Star Pattern.c

--- A Alphabet Flexible Size Star Pattern.c
+++ A Alphabet Flexible Size Star Pattern.c
@@ -2,7 +2,7 @@
 int main(){
 	int i,j,height;
 	scanf("%d",&height);
-// in flexible shapes always divide the lines
+	
 	for(i=1;i<=height;i++){
 		for(j=1;j<=height*2-1;j++){
 			if(j==height+1-i||j==height-1+i||i==(height+1)/2&&(j>(height+1)/2&&j<((height+1)/2)+height-1))\
